6e5011f23d3890ae5236408bf552269c6c590cd3
volodg <gorbenko.vova@gmail.com>
2015-05-28 22:06:18 +0300

    iAsync utils implementation

---
+++ iAsyncUtils/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iAsyncUtils
+//
+//  Created by Volodymyr on 28.05.15.
+//  Copyright (c) 2015 Volodymyr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
