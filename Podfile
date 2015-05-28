611dfc6af1293e5add2d19d57eeb487f77756433
volodg <gorbenko.vova@gmail.com>
2015-05-28 23:26:16 +0300

    iAsync utils implementation

--- Podfile
+++ Podfile
@@ -1,9 +1,6 @@
 platform :ios, '8.0'
 use_frameworks!
 
-target 'AppDaddyAPI' do
-pod 'Argo', '~> 1.0.1'
-pod 'FBSDKCoreKit', '~> 4.1.0'
-pod 'FBSDKLoginKit', '~> 4.1.0'
-pod 'FBSDKShareKit', '~> 4.1.0'
+target 'iAsyncUtils' do
+pod 'iAsync.utils', '~> 0.0.1'
 end
--- iAsyncUtils/AppDelegate.swift
+++ iAsyncUtils/AppDelegate.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
+import iAsync_utils
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
     }
 
