41678a00cd79e1bed36af5917c94b39d52b00730
volodg <gorbenko.vova@gmail.com>
2015-05-30 21:34:08 +0300

    iAsync utils implementation

--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 platform :ios, '8.0'
 use_frameworks!
 
-target 'iAsyncUtils' do
+target 'iAsync.utils' do
 pod 'iAsync.utils', '~> 0.0.1'
 end
---
+++ Pods/Target Support Files/Pods-iAsync.utils/Pods-iAsync.utils-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_iAsync_utilsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iAsync_utilsVersionString[];
+
