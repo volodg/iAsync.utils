d2ee2b3b4d6536c1ff365dd619179af21fc0fd03
vlg <gorbenko.vova@gmail.com>
2015-08-11 17:06:14 +0300

    refactor

--- iAsync.utils/iAsync.utils.h
+++ iAsync.utils/iAsync.utils.h
@@ -2,8 +2,8 @@
 //  iAsync.utils.h
 //  iAsync.utils
 //
-//  Created by Volodymyr on 31.05.15.
-//  Copyright (c) 2015 Volodymyr. All rights reserved.
+//  Created by Vladimir Gorbenko on 31.05.15.
+//  Copyright (c) 2015 Vladimir Gorbenko. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
