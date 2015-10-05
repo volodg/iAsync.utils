bde64f2b66d9abc424188bf4d93c535ec4c06620
vlg <gorbenko.vova@gmail.com>
2015-10-05 13:11:32 +0300

    test

--- iAsync.utils/Helpers.swift
+++ iAsync.utils/Helpers.swift
@@ -14,10 +14,3 @@ public func synced<R>(lock: AnyObject, @noescape _ closure: () -> R) -> R {
     objc_sync_exit(lock)
     return result
 }
-
-public func foldr<A : SequenceType, B>(xs: A, zero: B, f: (A.Generator.Element, () -> B) -> B) -> B {
-    var g = xs.generate()
-    var next: () -> B = {zero}
-    next = { return g.next().map {x in f(x, next)} ?? zero }
-    return next()
-}
