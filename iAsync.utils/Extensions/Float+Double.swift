e3d9d4d423289f925c18649b55c641d9149acee9
vlg <gorbenko.vova@gmail.com>
2015-12-09 15:51:27 +0300

    test

---
+++ iAsync.utils/Extensions/Float+Double.swift
@@ -0,0 +1,17 @@
+//
+//  Float+Double.swift
+//  iAsync_utils
+//
+//  Created by Gorbenko Vladimir on 09.12.15.
+//  Copyright (c) 2015 EmbeddedSources. All rights reserved.
+//
+
+import Foundation
+
+extension Float {
+
+    public func toDouble() -> Double {
+
+        return Double(self)
+    }
+}
