21cf655a00f449dab6d28ac2a3d51ea5c2ede79e
vlg <gorbenko.vova@gmail.com>
2016-02-18 20:41:29 +0200

    small refactor

--- iAsync.utils/Dictionary/HashableDictionary.swift
+++ iAsync.utils/Dictionary/HashableDictionary.swift
@@ -9,29 +9,29 @@
 import Foundation
 
 public struct HashableDictionary<Key : Hashable, Value: Equatable> : Hashable, CustomStringConvertible {
-    
+
     public private(set) var dict = [Key:Value]()
-    
+
     public var hashValue: Int {
         return dict.count
     }
-    
+
     public init(_ dict: [Key:Value]) {
-        
+
         self.dict = dict
     }
-    
+
     public init() {
-    
+
         self.init([Key:Value]())
     }
-    
+
     public var description: String {
         return "JUtils.HashableDictionary: \(dict)"
     }
 }
 
 public func ==<Key : Hashable, Value: Equatable>(lhs: HashableDictionary<Key, Value>, rhs: HashableDictionary<Key, Value>) -> Bool {
-    
+
     return lhs.dict == rhs.dict
 }
